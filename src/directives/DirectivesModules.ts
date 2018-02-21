9627a772e38f5b9b7e015f7af2c54af1e393d948
Nayan <nayan.dabhi@sphererays.net>
2018-02-21 13:38:46 +0530

    Add shadow directive in project

--- src/directives/DirectivesModules.ts
+++ src/directives/DirectivesModules.ts
@@ -1,11 +1,18 @@
 import { NgModule } from "@angular/core"
 
-import { Ionic2MaskDirective } from './ionic2-mask-directive'
+import { Ionic2MaskDirective } from './ionic2-mask-directive';
+import { ShadowDirective } from './shadow-directive';
 
 @NgModule({
-  declarations: [Ionic2MaskDirective],
-  exports: [Ionic2MaskDirective],
-  imports: []
+  declarations: [
+    Ionic2MaskDirective,
+    ShadowDirective
+  ],
+  imports: [],
+  exports: [
+    Ionic2MaskDirective,
+    ShadowDirective
+  ]
 })
 
 export class DirectivesModule {
